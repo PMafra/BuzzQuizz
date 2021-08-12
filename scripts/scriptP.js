53112356aa2ecd81fa9c64c8db6c9f3a34498486
Pedro Mafra <pmafrarm@gmail.com>
2021-08-12 15:08:49 -0300

    Selected quizz-page - rendering questions from server added

--- scripts/scriptP.js
+++ scripts/scriptP.js
@@ -22,11 +22,11 @@ function renderAllQuizzes (response) {
     for (let i = 0; i < response.data.length; i++) {
         let allQuizzesContainer = document.querySelector(".quizz-list .all-quizzes");
         allQuizzesContainer.innerHTML += 
-        `<div class="quizz-layout quizz-box" onclick="changePages(this); renderQuizzSelected(this)">
+        `<div class="quizz-layout quizz-box" onclick="changePages(this); renderBanner(this); getQuizz(this)">
             <img class="img-background" src="${response.data[i].image}">
             <div class="gradient-container">
             </div>
-            <p class="description">Alguma descrição do quizz aaaa afojnasdfsan asdofnasdojn asdofnn</p>
+            <p class="description">${response.data[i].title}</p>
         </div>`
     }
 }
@@ -60,15 +60,43 @@ function refreshPage () {
 
 // QUIZZ PAGE
 
-function renderQuizzSelected (element) {
+function renderBanner (element) {
     let banner = element.innerHTML;
     document.querySelector(".banner").innerHTML = banner;
 }
 
-function teste () {
-    let teste = axios.get(SERVER_URL_QUIZZES);
 
-    teste.then(function (data) {console.log(data)});
-    teste.catch(function (error) {console.log(error)});
+let quizzSrc;
+function getQuizz (element) {
+    quizzSrc = element.firstElementChild.src;
+
+    let allQuizzes = axios.get(SERVER_URL_QUIZZES);
+    allQuizzes.then(renderQuizzQuestions);
+    allQuizzes.catch(function (error) {console.log(error)});
 }
-teste();
+
+function renderQuizzQuestions (response) {
+
+    for (let i = 0; i < response.data.length; i++) {
+        if (quizzSrc === response.data[i].image) {
+            for (let j = 0; j < response.data[i].questions.length; j++) {
+                page.innerHTML +=
+                `<div class="question-container" id="q${j}">
+                    <div class="question-box">
+                        <span class="question">${response.data[i].questions[j].title}</span>
+                    </div>
+                    <div class="answers-box">
+                    </div>
+                </div>`
+                for (let k = 0; k < response.data[i].questions[j].answers.length; k++) {
+                    let questionPrinted = document.getElementById(`q${j}`);
+                    questionPrinted.lastElementChild.innerHTML += 
+                        `<div class="answer-option">
+                            <img src="${response.data[i].questions[j].answers[k].image}">
+                            <span class="legend">${response.data[i].questions[j].answers[k].text}</span>
+                        </div>`
+                }
+            }      
+        }    
+    }
+}
