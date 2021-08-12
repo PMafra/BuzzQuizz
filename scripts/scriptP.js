adf60fa72ddbae6b35cbc1deefbfc22045f33ff9
Pedro Mafra <pmafrarm@gmail.com>
2021-08-12 15:13:40 -0300

    Resolving conflicts

--- scripts/scriptP.js
+++ scripts/scriptP.js
@@ -65,7 +65,6 @@ function renderBanner (element) {
     document.querySelector(".banner").innerHTML = banner;
 }
 
-
 let quizzSrc;
 function getQuizz (element) {
     quizzSrc = element.firstElementChild.src;
